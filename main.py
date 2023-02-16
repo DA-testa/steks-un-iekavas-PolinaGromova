76cc039c82708b36c7287d9f3a6823bc656b7305
PolinaGromova <124886886+PolinaGromova@users.noreply.github.com>
2023-02-16 11:07:41 +0000

    Polina Gromova Upgrade

--- main.py
+++ main.py
@@ -13,17 +13,16 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i))
-
+            opening_brackets_stack.append(Bracket(next,i+1))
         if next in ")]}":
             if not opening_brackets_stack:
                 return i+1
-            a = opening_brackets_stack.pop()
-            if not are_matching:
+            if not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
+            opening_brackets_stack.pop()
 
     if opening_brackets_stack:
-        return opening_brackets_stack[-1].position + 1
+        return opening_brackets_stack[-1].position
     else: return "Success"
 
 def main():
